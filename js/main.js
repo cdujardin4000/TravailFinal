07b60798bd4ffbfd08d7c39ba0c5ec2019dc697b
cdujardin4000 <cdujardin4000@gmail.com>
2020-04-18 11:57:29 +0200

    indexok

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const   menu        =   document.querySelector('.l-main-menu');
+const   btn         =   document.querySelector('.nav-hamburger');
+
+document.querySelectorAll('.nav-hamburger').forEach(btn => {
+    btn.addEventListener('click', e => {
+        btn.classList.toggle('active');
+        menu.classList.toggle('open');
+    });
+    
+});
+document.querySelectorAll('.link').forEach(item => {
+    item.addEventListener('click', e => {
+        menu.classList.toggle('open');
+        btn.classList.toggle('active');
+    });
+});
